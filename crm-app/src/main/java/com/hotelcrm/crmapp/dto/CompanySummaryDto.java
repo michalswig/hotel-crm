18525efa5382a48391bf63792e508c04beb39bba
michalswig <mike@data.pl>
2025-06-28 23:15:14 +0200

    add inserts in liquibase and query for dashboard - CompanySummaryDto

---
+++ crm-app/src/main/java/com/hotelcrm/crmapp/dto/CompanySummaryDto.java
@@ -0,0 +1,71 @@
+package com.hotelcrm.crmapp.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class CompanySummaryDto {
+    Long companyId;
+    String companyName;
+    String ownerUsername;
+    LocalDateTime lastContactDate;
+    BigDecimal totalRevenueYTD;
+    BigDecimal totalRevenueLY;
+
+    public CompanySummaryDto(Long companyId, String companyName, String ownerUsername, LocalDateTime lastContactDate, BigDecimal totalRevenueYTD, BigDecimal totalRevenueLY) {
+        this.companyId = companyId;
+        this.companyName = companyName;
+        this.ownerUsername = ownerUsername;
+        this.lastContactDate = lastContactDate;
+        this.totalRevenueYTD = totalRevenueYTD;
+        this.totalRevenueLY = totalRevenueLY;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getOwnerUsername() {
+        return ownerUsername;
+    }
+
+    public void setOwnerUsername(String ownerUsername) {
+        this.ownerUsername = ownerUsername;
+    }
+
+    public BigDecimal getTotalRevenueYTD() {
+        return totalRevenueYTD;
+    }
+
+    public void setTotalRevenueYTD(BigDecimal totalRevenueYTD) {
+        this.totalRevenueYTD = totalRevenueYTD;
+    }
+
+    public BigDecimal getTotalRevenueLY() {
+        return totalRevenueLY;
+    }
+
+    public void setTotalRevenueLY(BigDecimal totalRevenueLY) {
+        this.totalRevenueLY = totalRevenueLY;
+    }
+
+    public LocalDateTime getLastContactDate() {
+        return lastContactDate;
+    }
+
+    public void setLastContactDate(LocalDateTime lastContactDate) {
+        this.lastContactDate = lastContactDate;
+    }
+}
--- crm-app/src/main/java/com/hotelcrm/crmapp/repository/CompanyRepository.java
+++ crm-app/src/main/java/com/hotelcrm/crmapp/repository/CompanyRepository.java
@@ -1,10 +1,49 @@
 package com.hotelcrm.crmapp.repository;
 
+import com.hotelcrm.crmapp.dto.CompanySummaryDto;
 import com.hotelcrm.crmapp.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Integer>, JpaSpecificationExecutor<Company> {
+
+    @Query("""
+            SELECT new com.hotelcrm.crmapp.dto.CompanySummaryDto(
+                        c.id,
+                        c.name,
+                        u.username,
+                        (
+                        SELECT MAX(i.interactionDate)
+                        FROM Interaction i
+                        WHERE i.contactPerson.company = c
+                                    ),
+                        (
+                        SELECT COALESCE(SUM(e.estimatedTotalGrossRevenue), 0)
+                        FROM Event e
+                        WHERE e.company = c
+                        AND EXTRACT(YEAR FROM e.eventDate) = :ytdYear
+                                    ),
+                        (
+                        SELECT COALESCE(SUM(e.estimatedTotalGrossRevenue), 0)
+                        FROM Event e
+                        WHERE e.company = c
+                        AND EXTRACT(YEAR FROM e.eventDate) = :lyYear
+                                                  )
+                                                               )
+                        from Company c
+                        join c.createdBy u
+            
+            """)
+    List<CompanySummaryDto> fetchCompanySummaryTable(
+            @Param("ytdYear") int ytdYear,
+            @Param("lyYear") int lyYear
+    );
+
+
 }
